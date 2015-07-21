b8bb267a314c05b3d0f91f92412124ff763399f7
Lais Varejão <laisvarejao@gmail.com>
2015-07-21 15:31:29 -0400

    Refactors according to Ben's code review.

--- lib/instacloud/fetch.rb
+++ lib/instacloud/fetch.rb
@@ -3,15 +3,19 @@ require_relative 'image'
 
 module Fetch
 	
-	def self.popular_media
-		client = Instagram.client()
-	  for media_item in client.media_popular
+  def self.popular_media
+    client = Instagram.client()
+	for media_item in client.media_popular
+	  begin
 	    Image.create(
-	    	:instagram_id => media_item.id,
-	    	:link => media_item.link,
-	    	:image => media_item.images.standard_resolution.url,
-	    	:date => media_item.created_time,
-	    	:likes => media_item.likes.count)
+		  :instagram_id => media_item.id,
+		  :link => media_item.link,
+		  :image => media_item.images.standard_resolution.url,
+		  :date => media_item.created_time,
+		  :likes => media_item.likes.count)
+	  rescue Sequel::UniqueConstraintViolation => e
+	    puts "caught exception #{e}! ohnoes!"
 	  end
-	end
+    end
+  end
 end
--- lib/instacloud/image.rb
+++ lib/instacloud/image.rb
@@ -1,4 +1,16 @@
 require '../../config/database'
 
 class Image < Sequel::Model
+  
+  def today
+    Image.where{date > unix_formatted_date_for_today}
+  end
+
+  private 
+
+  def unix_formatted_date_for_today
+  	Date.today.to_time.to_i
+  end
+  
 end
+
