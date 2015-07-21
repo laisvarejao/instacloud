f694e6729a8bdeb6147a19bbfb58313a06f41a8b
Lais Varejão <laisvarejao@gmail.com>
2015-07-21 15:32:14 -0400

    Changes database to include unique field.

--- config/database.rb
+++ config/database.rb
@@ -2,6 +2,6 @@ require 'sequel'
 require_relative 'environment'
 
 if ENV['DATABASE_URL']
-	DB = Sequel.connect ENV['DATABASE_URL']
-	puts 'Connected to the database.'
+    DB = Sequel.connect ENV['DATABASE_URL']
+    puts 'Connected to the database.'
 end
