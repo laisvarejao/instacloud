5ea0b56324ebf0557dc55f390bbfc38e6e87b509
Lais Varejão <laisvarejao@gmail.com>
2015-07-23 16:11:22 -0400

    Adds update function, in case of duplicate constraint exception.

--- lib/instacloud/fetch.rb
+++ lib/instacloud/fetch.rb
@@ -5,18 +5,18 @@ module Fetch
 	
   def self.popular_media
     client = Instagram.client()
-	for media_item in client.media_popular
-	  begin
-	    Image.create(
-		  :instagram_id => media_item.id,
-		  :link => media_item.link,
-		  :image => media_item.images.standard_resolution.url,
-		  :date => media_item.created_time,
-		  :likes => media_item.likes[:count])
-	    puts '.'
-	  rescue Sequel::UniqueConstraintViolation => e
-	    puts "caught exception #{e}! ohnoes!"
-	  end
+  	for media_item in client.media_popular
+  	  begin
+  	    Image.create(
+  		  :instagram_id => media_item.id,
+  		  :link => media_item.link,
+  		  :image => media_item.images.standard_resolution.url,
+  		  :date => media_item.created_time,
+  		  :likes => media_item.likes[:count])
+  	  rescue Sequel::UniqueConstraintViolation => e
+  	    puts "caught exception #{e}! oh no!"
+  	    Image.update_likes(media_item.id, media_item.likes[:count])
+  	  end
     end
   end
 end
--- lib/instacloud/image.rb
+++ lib/instacloud/image.rb
@@ -1,12 +1,18 @@
 require_relative '../../config/database'
 
 class Image < Sequel::Model
-  
-  def popular_today
-    Image.where{date > Date.today.to_time.to_i}.reverse_order(:likes).limit(20)
+
+  dataset_module do
+    def update_likes(instagram_id, likes) 
+      where(:instagram_id => instagram_id).update(:likes => likes)
+    end
   end
 
-  private 
+  def popular_today
+    Image.where{date > Date.today.to_time.to_i}.reverse_order(:likes).limit(20)
+  end 
+
+  private
 
   def unix_formatted_date_for_today
   	Date.today.to_time.to_i
