c74dd090b2e6307f98c0dfe9c6a5d3edd669a70a
Lais Varejão <laisvarejao@gmail.com>
2015-08-11 14:26:32 -0400

    Adds popular images now.

--- lib/instacloud/fetch.rb
+++ lib/instacloud/fetch.rb
@@ -1,5 +1,6 @@
 require_relative '../../config/instagram'
 require_relative 'image'
+require_relative 'raw_image'
 
 module Fetch
 	
@@ -19,4 +20,24 @@ module Fetch
   	  end
     end
   end
+
+  def self.popular_now
+    images = []
+    client = Instagram.client()
+    for media_item in client.media_popular
+      image = RawImage.new(
+        media_item.link,
+        media_item.images.standard_resolution.url,
+        media_item.likes[:count]
+      )
+      images << image
+    end
+    images
+  end
+
+  def self.total_likes_most_popular(images)
+    sorted_images = images.sort_by(&:likes)
+    sorted_images.last.likes
+  end
+
 end
