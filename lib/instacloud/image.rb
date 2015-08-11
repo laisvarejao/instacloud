a9f4ab298b260eb77be7bda35397b23e1847db5c
Lais Varejão <laisvarejao@gmail.com>
2015-08-11 14:49:37 -0400

    Removes class for deploy.

--- lib/instacloud/image.rb
+++ lib/instacloud/image.rb
@@ -1,23 +1,23 @@
-require_relative '../../config/database'
+# require_relative '../../config/database'
 
-class Image < Sequel::Model
+# class Image < Sequel::Model
 
-  dataset_module do
-    def update_likes(instagram_id, likes) 
-      where(:instagram_id => instagram_id).update(:likes => likes)
-    end
-  end
+#   dataset_module do
+#     def update_likes(instagram_id, likes) 
+#       where(:instagram_id => instagram_id).update(:likes => likes)
+#     end
+#   end
 
-  def popular_today
-    today = unix_formatted_date_for_today
-    Image.where{date > today}.reverse_order(:likes).limit(20).all
-  end
+#   def popular_today
+#     today = unix_formatted_date_for_today
+#     Image.where{date > today}.reverse_order(:likes).limit(20).all
+#   end
 
-  private
+#   private
 
-  def unix_formatted_date_for_today
-  	Date.today.to_time.to_i
-  end
+#   def unix_formatted_date_for_today
+#   	Date.today.to_time.to_i
+#   end
   
-end
+# end
 
