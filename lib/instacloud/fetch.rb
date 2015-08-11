60adf1a469e6125810bf6cc6c73141b152a3a975
Lais Varejão <laisvarejao@gmail.com>
2015-08-11 14:46:00 -0400

    Removes database dependencies for deploy.

--- lib/instacloud/fetch.rb
+++ lib/instacloud/fetch.rb
@@ -1,25 +1,25 @@
 require_relative '../../config/instagram'
-require_relative 'image'
+# require_relative 'image'
 require_relative 'raw_image'
 
 module Fetch
 	
-  def self.popular_media
-    client = Instagram.client()
-  	for media_item in client.media_popular
-  	  begin
-  	    Image.create(
-  		  :instagram_id => media_item.id,
-  		  :link => media_item.link,
-  		  :image => media_item.images.standard_resolution.url,
-  		  :date => media_item.created_time,
-  		  :likes => media_item.likes[:count])
-  	  rescue Sequel::UniqueConstraintViolation => e
-  	    puts "caught exception #{e}! oh no!"
-  	    Image.update_likes(media_item.id, media_item.likes[:count])
-  	  end
-    end
-  end
+  # def self.popular_media
+  #   client = Instagram.client()
+  # 	for media_item in client.media_popular
+  # 	  begin
+  # 	    Image.create(
+  # 		  :instagram_id => media_item.id,
+  # 		  :link => media_item.link,
+  # 		  :image => media_item.images.standard_resolution.url,
+  # 		  :date => media_item.created_time,
+  # 		  :likes => media_item.likes[:count])
+  # 	  rescue Sequel::UniqueConstraintViolation => e
+  # 	    puts "caught exception #{e}! oh no!"
+  # 	    Image.update_likes(media_item.id, media_item.likes[:count])
+  # 	  end
+  #   end
+  # end
 
   def self.popular_now
     images = []
